bfb379c9e0173b902698b581f559da9865b13446
LuisPedroFarias <161033603+LuisPedroFarias@users.noreply.github.com>
2024-05-03 12:24:39 +0000

    Tipagens

---
+++ 1.Tipos de dados/2.Tipagem dinâmica.py
@@ -0,0 +1,18 @@
+# Tipagem dinâmica.
+
+
+#Atribuindo váriaveis
+nome = "Marta"
+idade = 20
+peso = 59.003
+
+#Exibindo dados.
+#Simples.
+print(nome)
+print(idade)
+print(peso)
+
+#Formatado.
+print(F"Nome: {nome}")
+print(F"Idade: {idade}")
+print(F"Peso: {peso}")
---
+++ 1.Tipos de dados/3.Tipagem estática.py
@@ -0,0 +1,11 @@
+#Tipagem estática.
+
+#Atribuindo váriaveis
+nome = str = "Marta"
+idade = int = 20
+peso = float = 59.003
+
+#Exibindo dados.
+print(F"Nome: {nome}")
+print(F"Idade: {idade}")
+print(F"Peso: {peso}")
